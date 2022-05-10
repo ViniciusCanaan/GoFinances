351b1c0db21f0a9dd06f20214c99bf7e299d01b5
Vinicius Canaan <60820733+ViniciusCanaan@users.noreply.github.com>
2022-05-09 21:18:03 -0300

    Criando Header
    Criando Header e componentes de exibição de dados do usuário

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,   
+    Icon
+} from "./styles";
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: "https://avatars.githubusercontent.com/u/60820733?v=4" }} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Vinicius</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power"/>
+                </UserWrapper>
+               
+            </Header>
+        </Container>
+    );
+}
