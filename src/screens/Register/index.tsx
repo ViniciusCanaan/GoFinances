a7ca51a6c887a90391eedc82f70f74d72382cf6b
Vinicius Canaan <60820733+ViniciusCanaan@users.noreply.github.com>
2022-05-17 21:42:26 -0300

    Criando tela de cadastro

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Container, Header, Title, Form, Fields } from './styles';
+
+import { Input } from '../../components/Forms/input';
+import { Button } from '../../components/Forms/Button';
+
+export function Register() {
+    return (
+        <Container>
+            <Header>
+                <Title>Cadastro</Title>
+            </Header>
+
+            <Form>
+                <Fields>
+                    <Input
+                        placeholder='Nome'
+                    />
+                    <Input
+                        placeholder='Preço'
+                    />
+                </Fields>
+                <Button title="Enviar" />
+            </Form>
+        </Container>
+    );
+} 
